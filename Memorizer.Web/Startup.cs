dd4b719b407248cfd1f8163afd6c42bfb31c3cfb
RaZoR <razorbohan@yahoo.com>
2019-07-23 09:24:05 +0300

    add CORS for React dev port

--- Memorizer.Web/Startup.cs
+++ Memorizer.Web/Startup.cs
@@ -131,7 +131,7 @@ namespace Memorizer.Web
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
-                    builder => builder.WithOrigins("http://localhost:4200")
+                    builder => builder.WithOrigins("http://localhost:3000", "http://localhost:4200")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials());
